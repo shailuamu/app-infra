4b0be14a187ca2332a4faa6bd5d8ec2fc8132827
Shailesh Gargade <ShaileshG@talentica-all.com>
2017-07-05 14:58:53 +0530

    changed manage report script and added username as argument

--- scripts/manage_reports.sh
+++ scripts/manage_reports.sh
@@ -3,6 +3,7 @@ SOURCE_PATH=$2
 DESTINATION=$3
 BUILD_COUNTER_VAR=$4
 HOST_ADD=$5
+HOST_USER=$6
 
 echo 'Starting copying Reports'
 echo $HOST_ADD
@@ -11,6 +12,6 @@ eval BUILD_COUNTER='$'$BUILD_COUNTER_VAR
 
 echo $BUILD_COUNTER
 
-scp -o StrictHostKeyChecking=no -r $SOURCE_PATH ec2-user@$HOST_ADD:
+scp -o StrictHostKeyChecking=no -r $SOURCE_PATH $HOST_USER@$HOST_ADD:
 
-ssh -o StrictHostKeyChecking=no ec2-user@$HOST_ADD 'bash -s' < move_to_folder.sh $FILE_NAME $DESTINATION $BUILD_COUNTER
+ssh -o StrictHostKeyChecking=no $HOST_USER@$HOST_ADD 'bash -s' < move_to_folder.sh $FILE_NAME $DESTINATION $BUILD_COUNTER
---
+++ scripts/manage_reports_readme.txt
@@ -0,0 +1,16 @@
+Use this command to copy reports artifact -
+
+manage_reports.sh
+jacoco
+/var/lib/go-agent/pipelines/BuildBackendApplication/web/service/target/site/jacoco
+/home/ec2-user/projects/reports-viewer/static/reports/junit
+GO_PIPELINE_COUNTER
+13.229.31.126
+ec2-user
+
+arg-1 - Name of file/folder
+arg-2 - Source_Path
+arg-3 - Destination_Path to copy
+arg-4 - Build Counter
+arg-5 - HOST IP
+arg-6 - HOST User
